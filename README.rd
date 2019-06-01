1ee0cd8386143b7ed05b3d653cbf9f5b9aed3579
John <jbertlik@gmail.com>
2019-06-01 15:42:59 -0400

    Just made a simple demo to see how I can access my webcam via OpenCV2 and Python.

---
+++ README.rd
@@ -0,0 +1,3 @@
+I am following one of the tutorials from this course: https://www.udemy.com/the-python-mega-course/
+
+This program uses the OpenCV2 Library to communicate with my webcam to capture video.
---
+++ video_capture.py
@@ -0,0 +1,25 @@
+import cv2, time
+
+video=cv2.VideoCapture(0)
+
+how_many_frames=0
+
+while True:
+    how_many_frames=how_many_frames+1
+    check, frame=video.read()
+
+    print(check)
+    print(frame)
+
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    # time.sleep(3)
+    cv2.imshow("Capture1",gray)
+
+    key=cv2.waitKey(1)
+
+    if key==ord('q'):
+        break
+print("There were " + str(how_many_frames) + " many frames in this video.")
+
+video.release()
+cv2.destroyAllWindows()
